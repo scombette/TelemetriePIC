e84e3e859144d527fe237f504c9e2fe977a8947f
Sébastien Combette <sebastien.combette@gmail.com>
2016-12-27 12:03:28 +0100

    Ajout description projet

--- TelemetriePIC.X/main.c
+++ TelemetriePIC.X/main.c
@@ -1,16 +1,14 @@
-/* 
- * File:   main.c
- * Author: Sébastien
- *
- * Created on 27 décembre 2016, 09:41
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/*
+/******************************************************************************
+ *                          TelemetriePIC/main.c
+ *  Fichier main du programme de la carte telemetrie
+ *  
+ *  Config : PIC18F26K80, liaison CAN avec MCP 2551, RS232 avec Radio et GPS, 
+ *  SPI avec carte SD
  * 
- */
+ *  Brief : Ce programme gère la communication avec la telemetrie et les logs
+ * 
+ * ***************************************************************************/
+
 int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
