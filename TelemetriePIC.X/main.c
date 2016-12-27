185a70dc5689669b97c49de99f016d028d347a94
Sébastien Combette <sebastien.combette@gmail.com>
2016-12-27 11:58:33 +0100

    Add project files
    Adding the first project files

---
+++ TelemetriePIC.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Sébastien
+ *
+ * Created on 27 décembre 2016, 09:41
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
